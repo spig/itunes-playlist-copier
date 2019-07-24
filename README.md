d0359d363f44ec1bda2d9b62556b751785c91dd8
Steve Spigarelli <steve.spigarelli@gmail.com>
2019-07-23 22:09:06 -0600

    first commit - code functional and not pretty - but works

---
+++ README.md
@@ -0,0 +1,5 @@
+# Copy music from iTunes Playlist Export file into another destination
+
+* Run with python3 copy-itunes-playlist-music.py <playlist-exported-from-itunes> <destination-folder>
+
+For debug mode change code line saying `debug = False` to `debug = True`
---
+++ copy-itunes-playlist-music.py
@@ -0,0 +1,65 @@
+#!/usr/local/bin/python3
+###########
+## 
+## This script copies music files from an iTunes playlist to another location
+## 
+## argument 1 - the itunes playlist file
+## argument 2 - the location to copy music to
+## 
+###############
+
+import sys
+import os
+import csv
+from shutil import copyfile
+
+def makeUniqueFile(path):
+    origName, origExt = os.path.splitext(path)
+    counter = 1
+    while True:
+        if os.path.exists(path):
+            path = origName + '_' + str(counter) + origExt
+            print(f"path {path}")
+            counter += 1
+        else:
+            print(f"returning {path}")
+            return path
+
+debug = True
+
+if len(sys.argv) < 3:
+    print("usage: " + sys.argv[0] + " <itunes-playlist-export-txt-file> <copy-destination>")
+    exit(1)
+
+destinationFolder = sys.argv[2]
+
+if not os.access(destinationFolder, os.W_OK):
+    print("Destination folder may not exist or allow us to write files")
+    exit(1)
+
+with open(sys.argv[1], newline='', encoding='utf-16') as f:
+    next(f)
+    reader = csv.reader(f, delimiter='\t')
+    for row in reader:
+        musicFile = row[-1]
+        if musicFile == "":
+            print(f'Could not find {row[1]}')
+            continue
+        musicFile = musicFile[musicFile.find('/'):]
+        print(f'musicfile: {musicFile}')
+        destFile = os.path.split(musicFile)[1]
+        destFile = "test.mp3"
+        destination = os.path.join(destinationFolder, destFile)
+        print(f'Destination: {destination}')
+        if os.access(musicFile, os.R_OK):
+            if debug:
+                destination = makeUniqueFile(destination)
+                print(f'would have copied {musicFile} to {destination}')
+            else:
+                destination = makeUniqueFile(destination)
+                print(f'copying {musicFile} to {destination}')
+                copyfile(musicFile, destination)
+        elif debug:
+            print(f"could not locate {musicFile}")
+            print("debug ---row---")
+            print(row)
